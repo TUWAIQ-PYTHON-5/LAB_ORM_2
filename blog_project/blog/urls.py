570a32c305edb4c667be76c65bc7e9cfe5722806
Anfal Ali <name@gmail.com>
2023-02-08 21:25:56 +0300

    LAB_ORM_2

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -3,13 +3,13 @@ from . import views
 app_name='blog'
 
 urlpatterns = [
-    path('add/', views.add_blog, name='add_blog_page'),
-    path('update_blog/<blog_id>/', views.update_blog,name="update_page"),
     path('',views.home,name='home_page'),
-    path("delete/<blog_id>/", views.delete_blog, name="delete_blog"),
-    path('read/',views.read_blog,name='read_blog_page'),
+    path('add/', views.add_blog, name='add_blog_page'),
+    path("latest/", views.latest_blog, name="latest_blog_page"),
     path("details/<blog_id>/", views.blog_detail, name="blog_detail"),
-    path('search/',views.search_title,name="search_page"),
+    path("update/<blog_id>/", views.update_blog, name="update_blog"),
+    path("delete/<blog_id>/", views.delete_blog, name="delete_blog"),
+    path("search/", views.search_blog, name="search_blog"),
     
    
 
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -2,21 +2,6 @@ from django.shortcuts import render,redirect
 from django.http import HttpRequest
 from .models import Blog
 
-def update_blog(request:HttpRequest,blog_id):
-    blog= Blog.objects.get(id=blog_id)
-    blog.publish_date = blog.publish_date.isoformat #to make it compatible with input value in html
-    if request.method == "POST":
-        blog.Title = request.POST["Title"]
-        blog.Content= request.POST["Content"]
-        blog.is_published = request.POST["is_publishe"]
-        blog.publish_date = request.POST["publish_date"]
-
-        blog.save()
-        return redirect("blog:read_blog_page")
-
-    return render(request, "blog/update_blog.html", {"blog" : blog})
-
-
 
 def home(request:HttpRequest):
     return render(request,'blog/base.html')
@@ -26,49 +11,47 @@ def add_blog(request:HttpRequest):
     if request.method == "POST":
        new_blog=Blog(Title=request.POST["Title"],Content=request.POST["Content"],is_published = request.POST["is_published"])
        new_blog.save()
-       return redirect('blog:read_blog_page')
+       return redirect('blog:latest_blog_page')
 
     return render(request, "blog/add_blog.html")
-def update_blog(request:HttpRequest,blog_id):
-    blog= Blog.objects.get(id=blog_id)
-    blog.publish_date = blog.publish_date.isoformat
-    if request.method == "POST":
-        blog.Title = request.POST["Title"]
-        blog.Content= request.POST["Content"]
-        blog.is_published = request.POST["is_publishe"]
-        blog.publish_date = request.POST["publish_date"]
 
-        blog.save()
-        return redirect("blog:add_blog_page")
+def latest_blog(request : HttpRequest):
 
-    return render(request, "blog/update_blog.html", {"blog" : blog})
-def delete_blog(request : HttpRequest, blog_id):
-    blog = Blog.objects.get(id=blog_id)
-    blog.delete()
-    return redirect("blog:read_blog_page")
-def read_blog(request:HttpRequest):
-    read_blog = Blog.objects.all()
+    latest_blog = Blog.objects.all()
 
-    context = {"read_blog" :read_blog}
-    return render(request,"blog/index.html", context)
+    context = {"latest_blog" : latest_blog}
+    return render(request, "blog/index.html", context)
 
 
-def blog_detail(request : HttpRequest, blog_id):
+def blog_detail(request:HttpRequest, blog_id):
 
     blog = Blog.objects.get(id=blog_id)
     return render(request, "blog/blog_detail.html", {"blog" : blog})
-def search_title(request:HttpRequest):
-    if request.method=="POST":
-        search_title=request.POST['search_title']
-        searches=Blog.objects.filter(Title__contains=search_title)
-        context = {"search_title" : search_title,
-                   "searches":searches}
-        return render(request, "blog/search_blog.html", context)
-    else:
 
-       return render(request, "blog/search_blog.html", context)
+def update_blog(request : HttpRequest, blog_id):
 
+    blog = Blog.objects.get(id=blog_id)
+    blog.publish_date = blog.publish_date.isoformat 
+    if request.method == "POST":
+        blog.title = request.POST["Title"]
+        blog.Content = request.POST["Content"]
+        blog.is_published = request.POST["is_published"]
+        
 
+        blog.save()
+        return redirect("blog:latest_blog_page")
 
+    return render(request, "blog/update_blog.html", {"blog" : blog})
+def delete_blog(request : HttpRequest, blog_id):
+    blog= Blog.objects.get(id=blog_id)
+    blog.delete()
+    return redirect("blog:latest_blog_page")
+def search_blog(request:HttpRequest):
+     if request.method=="POST":
+        searched= request.POST['searched'] 
+        search_blog=Blog.objects.filter(Title__contains=searched)
 
-
+        return render(request, "blog/search.html", {'searched':searched,'search_blog':search_blog})
+     else:
+        return render(request, "blog/search.html", {})
+ 
