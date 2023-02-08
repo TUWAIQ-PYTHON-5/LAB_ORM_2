8dae8d5a998ca72e317e3f8a911c431af973e020
nada.abdullah <none.4292@gmail.com>
2023-02-09 02:39:18 +0300

    done the detail and update

--- Blog/main/urls.py
+++ Blog/main/urls.py
@@ -7,5 +7,11 @@ app_name = "main"
 urlpatterns = [
 
     path('', views.home_page, name="home_page"),
-    path("update/<post_id>/", views.update_post, name="update_post"),    
+    path("update/<post_id>/", views.update_post, name="update_post"), 
+    path("delete/<post_id>/", views.delete_post, name="delete_post"),
+    path("details/<post_id>/", views.post_detail, name="post_detail"),
+    path("search/", views.post_search, name="post_search"),    
+
+
+   
 ]
--- Blog/main/views.py
+++ Blog/main/views.py
@@ -6,19 +6,19 @@ from .models import Post
 
 
 def home_page (request : HttpRequest):
-    Posts = Post.objects.all()
-    context = {"posts" : Posts}
+    posts = Post.objects.all()
     if request.method == "POST":
         new_post = Post(title=request.POST["title"], Content=request.POST["content"], is_published=request.POST["is_published"])
         new_post.save()
+        print(request.POST["title"])
         return redirect("main:home_page")
+    context = {"posts" : posts}
     return render (request,"main/home.html", context)
 
 
 def update_post (request : HttpRequest, post_id):
-
     posts = Post.objects.get(id=post_id)
-    posts.publish_date = posts.publish_date.isoformat
+    posts.publish_date = posts.publish_date.isoformat 
     if request.method == "POST":
         posts.title = request.POST["title"]
         posts.Content = request.POST["content"]
@@ -26,8 +26,27 @@ def update_post (request : HttpRequest, post_id):
         posts.publish_date = request.POST["publish_date"]
 
         posts.save()
-        return redirect("main:home_page")
+        return redirect("main:post_detail")
 
-    return render(request,"main/update_post.html", {"posts" : posts})
+    return render(request,"main/update_post.html", {"post" : posts})
 
 
+
+def delete_post (request : HttpRequest, post_id):
+    posts = Post.objects.get(id=post_id)
+    posts.delete()
+    return redirect("main:home_page")
+
+
+
+def post_detail(request : HttpRequest, post_id):
+    post = Post.objects.get(id=post_id)
+
+    return render(request, "main/post_detail.html", {"post" : post})
+
+
+
+def post_search(request: HttpRequest):
+    posts = Post.objects.filter(name__contains=request.GET.get('search'))
+
+    return render(request, 'main/post_detail.html', {'posts':posts})
